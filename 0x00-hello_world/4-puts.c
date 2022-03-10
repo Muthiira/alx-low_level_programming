674da9d89364caed08479876ac10417a577ea3d8
Muthiira <calebmuchiri48@gmail.com>
2022-03-10 18:20:02 +0300

    puts" ""

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
+
+
