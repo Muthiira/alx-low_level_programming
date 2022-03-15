d2f40d02ce880adaff0ed982a4f3b2da38b9b412
Muthiira <calebmuchiri48@gmail.com>
2022-03-15 14:33:31 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,15 @@
 *main - print _putchar followed by a new line
 *always return 0 (success)
 */
-int main(void) 
+int main(void) {
+char *s = "_putchar";
+while(*s != '\0')
 {
-    char *s = "_putchar";
-    while(*s != '\0')
-    {
-       _putchar(*s);
-       s++; 
-    }
-    _putchar('\n');
-
-    return (0);
+_putchar(*s);
+s++; 
+}
+_putchar('\n');
+return (0);
 
 }
 
