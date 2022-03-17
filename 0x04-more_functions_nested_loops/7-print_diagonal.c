ee5f12defb8f1cc129275bb0b515d5a2e48706d7
Muthiira <calebmuchiri48@gmail.com>
2022-03-17 13:26:35 +0300

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Function that draws a diagonal line
+ * @n: Number of time the character
+ */
+void print_diagonal(int n);
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				putchar(' ');
+			}
+			putchar('\\');
+			putchar(10);
+		}
+	}
+	else
+	{
+		putchar(10);
+	}
+}
