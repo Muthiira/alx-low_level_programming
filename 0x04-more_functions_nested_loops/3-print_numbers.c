b7a4c6fdb8e88e8f4930d17c3078e0b7b872116f
Muthiira <calebmuchiri48@gmail.com>
2022-03-17 13:16:35 +0300

    bb

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,12 @@
  */
 void print_numbers(void)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + '0');
-	}
+    for (i = 0; i < 10; i++)
+    {
+        putchar(i + '0');
+    }
 
-	putchar('\n');
+    putchar('\n');
 }
