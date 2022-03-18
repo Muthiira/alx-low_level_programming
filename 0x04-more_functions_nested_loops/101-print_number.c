1bf3aef94d4e09e3142755abb66d718178351b9b
Muthiira <calebmuchiri48@gmail.com>
2022-03-18 11:08:51 +0300

    nn

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,21 +6,22 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+    unsigned int n1;
 
-	if (n < 0)
-	{
-		n1 = -n;
-		putchar('-');
-	} else
-	{
-		n1 = n;
-	}
+    if (n < 0)
+    {
+        n1 = -n;
+        putchar('-');
+    }
+    else
+    {
+        n1 = n;
+    }
 
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
+    if (n1 / 10)
+    {
+        print_number(n1 / 10);
+    }
 
-	putchar((n1 % 10) + '0');
+    putchar((n1 % 10) + '0');
 }
