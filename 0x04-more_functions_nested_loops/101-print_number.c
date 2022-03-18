97a689a0931eab7d40d98a497b0af1816c36e329
Muthiira <calebmuchiri48@gmail.com>
2022-03-18 13:02:23 +0300

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,19 @@
  */
 void print_number(int n)
 {
-	/*unsigned int num = n;*/
+	unsigned int num = n;
 
     /* print '-' in front of -ve integers and ensure n is +ve */
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		num *= -1;
 	}
 
 	/* if num has more than 1-digit, print preceding digit first */
-	if (n >= 10)
-		print_number(n / 10);
+	if (num >= 10)
+		print_number(num / 10);
 
-	_putchar('0' + (n % 10));
+	_putchar('0' + (num % 10));
 
 }
