393060ad00feb9c6ed160d2e5a7799786b829d19
Muthiira <calebmuchiri48@gmail.com>
2022-03-23 11:51:47 +0300

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int l1, l2, i;
-	/*strcat(dest, src);*/
-
+	/*int l1, l2, i;*/
+	strcat(dest, src);
+/*
 	l1 = strlen(dest);
 	l2 = strlen(src);
 	for (i = 0;i<=l2;i++)
 	{
 		dest[l1 + i] = src[i];
-	}
+	}*/
 	return (dest);
 }
