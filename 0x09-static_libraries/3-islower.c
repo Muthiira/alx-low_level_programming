8dcf4b8ac3394cded5a9d839c1e1c6019f03f546
Muthiira <calebmuchiri48@gmail.com>
2022-04-01 17:10:34 +0300

    new

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,24 +1,12 @@
 #include "main.h"
 
 /**
- * _islower - Function that checks for lowercase
- *
- * @c: The number to be checked
- *
- * Return: 1 if c is lowercase and 0 otherwise
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-	int n;
-
-	if (c >= 97 && c <= 122)
-	{
-		n = 1;
-	}
-	else
-	{
-		n = 0;
-	}
-
-	return (n);
+	return (c >= 'a' && c <= 'z');
 }
+
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,24 +1,11 @@
 #include "main.h"
 
 /**
- * _isalpha - Function that checks for alphabetic character
- *
- * @c: The number to be checked
- *
- * Return: 1 if c is a letter, lowercase or uppercase and 0 otherwise
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	int n;
-
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		n = 1;
-	}
-	else
-	{
-		n = 0;
-	}
-
-	return (n);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
