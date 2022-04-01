080b099f0e29c313a2b1efdde0dfd2fc8be294bb
Muthiira <calebmuchiri48@gmail.com>
2022-04-01 18:10:11 +0300

    n

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,23 +1,3 @@
 #include "main.h"
-
-/**
- * _puts - Function that prints a string
- * @str: String to be printed
- */
-void _puts(char *str)
-{
-	int i;
-
-	i = 0;
-
-	while (1)
-	{
-		if (*(str + i) != '\0')
-			_putchar(*(str + i));
-		else
-			break;
-
-		i++;
-	}
-	_putchar(10);
-}
+void _puts(char *s)
+{}
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,22 +1,3 @@
 #include "main.h"
-
-/**
- * _abs - Function that computes the absolute value of an integer
- *
- * @n: The number to be checked
- *
- * Return: absolute value
- */
 int _abs(int n)
-{
-	if (n < 0)
-	{
-		n *= -1;
-	}
-	else
-	{
-		;
-	}
-
-	return (n);
-}
+{}
