1069767fe577d8eb86d63b77452cace2929739bd
Muthiira <calebmuchiri48@gmail.com>
2022-04-28 21:21:20 +0300

    a

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  print_binary - prints binary representation of a number
+ * @n:
+ * 
+ */
+
+void print_binary(unsigned long int n)
+{
+
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  get_bit - function that returns value of bit 
+ * @index: index starting from 0
+ * 
+ * 
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+}
